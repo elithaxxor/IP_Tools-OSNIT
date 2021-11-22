4fcaa79fa9738fff5830edfacbb8e5f95850aa16
a-robot <86808805+elithaxxor@users.noreply.github.com>
2021-11-22 15:09:46 -0800

    Update simpleWebBrowserCLIENT.py

--- simpleWebBrowserCLIENT.py
+++ simpleWebBrowserCLIENT.py
@@ -1,22 +1,26 @@
 import socket
 
 ## The Client
-print(f'[+] Address: ')
-IP = input('')
-PORT = 80
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((IP, PORT))
-cmd = "GET f'{IP}'\r\n\r\n".encode() ## change \r\n to headers later".encode()
-clientSocket.send(cmd)
+class WebBrowserC:
+    print(f'[+] Address" ')
+    def webbrowser(self):
+        IP = input('')
+        PORT = 80
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientSocket.connect((IP, PORT))
+        cmd = "GET f'{IP}'\r\n\r\n".encode() ## change \r\n to headers later".encode()
+        clientSocket.send(cmd)
+        while True:
+            clientData = clientSocket.recv(512)
+            if len(clientData) < 1:
+                break
+            print(clientData.decode(), end='')
+
+        clientSocket.close()
 
 
-while True:
-    clientData = clientSocket.recv(512)
-    if len(clientData) < 1:
-        break
-    print(clientData.decode(), end='')
-
-clientSocket.close()
+browser = WebBrowserC()
+browser.webbrowser()
 
 
 
