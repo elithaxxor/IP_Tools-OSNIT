97e957d93aa6c04e926276341c2321a9d2f5f3a3
a-robot <86808805+elithaxxor@users.noreply.github.com>
2021-12-07 23:27:47 -0800

    Update simpleWebBrowserCLIENT.py

--- simpleWebBrowserCLIENT.py
+++ simpleWebBrowserCLIENT.py
@@ -2,6 +2,22 @@ import socket
 import hmtl.parser import HTMLParser 
 
 
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+parser = MyHTMLParser()
+parser.feed('<html><head><title>Test</title></head>'
+            '<body><h1>Parse me!</h1></body></html>')
+
 
 ## The Client
 class WebBrowserC:
@@ -12,6 +28,7 @@ class WebBrowserC:
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         clientSocket.connect((IP, PORT))
         cmd = "GET f'{IP}'\r\n\r\n".encode() ## change \r\n to headers later".encode()
+        ### ADD HEADERS HERE #### 
         clientSocket.send(cmd)
         while True:
             clientData = clientSocket.recv(512)
@@ -24,6 +41,7 @@ class WebBrowserC:
 
 browser = WebBrowserC()
 browser.webbrowser()
+parser()
 
 
 
