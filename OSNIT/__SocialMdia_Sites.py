3213ea38510389255981fac4dd8bf25f540ee0ca
a-robot <86808805+elithaxxor@users.noreply.github.com>
2021-12-12 07:54:30 -0800

    Create __SocialMdia_Sites.py

---
+++ OSNIT/__SocialMdia_Sites.py
@@ -0,0 +1,174 @@
+#import socialmedia
+#from socialmedia import USERNAME
+
+
+USERNAME = input(f'[sys] Enter Username:' )
+
+# INSTAGRAM
+instagram = f'https://www.instagram.com/{USERNAME}'
+# FACEBOOK
+facebook = f'https://www.facebook.com/{USERNAME}'
+#TWITTER
+twitter = f'https://www.twitter.com/{USERNAME}'
+# YOUTUBE
+youtube = f'https://www.youtube.com/{USERNAME}'
+# BLOGGER
+blogger = f'https://{USERNAME}.blogspot.com'
+# GOOGLE+
+google_plus = f'https://plus.google.com/s/{USERNAME}/top'
+# REDDIT
+reddit = f'https://www.reddit.com/user/{USERNAME}'
+# WORDPRESS
+wordpress = f'https://{USERNAME}.wordpress.com'
+# PINTEREST
+pinterest = f'https://www.pinterest.com/{USERNAME}'
+# GITHUB
+github = f'https://www.github.com/{USERNAME}'
+# TUMBLR
+tumblr = f'https://{USERNAME}.tumblr.com'
+# FLICKR
+flickr = f'https://www.flickr.com/people/{USERNAME}'
+# STEAM
+steam = f'https://steamcommunity.com/id/{USERNAME}'
+# VIMEO
+vimeo = f'https://vimeo.com/{USERNAME}'
+# SOUNDCLOUD
+soundcloud = f'https://soundcloud.com/{USERNAME}'
+# DISQUS
+disqus = f'https://disqus.com/by/{USERNAME}'
+# MEDIUM
+medium = f'https://medium.com/@{USERNAME}'
+# DEVIANTART
+deviantart = f'https://{USERNAME}.deviantart.com'
+# VK
+vk = f'https://vk.com/{USERNAME}'
+# ABOUT.ME
+aboutme = f'https://about.me/{USERNAME}'
+# IMGUR
+imgur = f'https://imgur.com/user/{USERNAME}'
+# FLIPBOARD
+flipboard = f'https://flipboard.com/@{USERNAME}'
+# SLIDESHARE
+slideshare = f'https://slideshare.net/{USERNAME}'
+# FOTOLOG
+fotolog = f'https://fotolog.com/{USERNAME}'
+# SPOTIFY
+spotify = f'https://open.spotify.com/user/{USERNAME}'
+# MIXCLOUD
+mixcloud = f'https://www.mixcloud.com/{USERNAME}'
+# SCRIBD
+scribd = f'https://www.scribd.com/{USERNAME}'
+# BADOO
+badoo = f'https://www.badoo.com/en/{USERNAME}'
+# PATREON
+patreon = f'https://www.patreon.com/{USERNAME}'
+# BITBUCKET
+bitbucket = f'https://bitbucket.org/{USERNAME}'
+# DAILYMOTION
+dailymotion = f'https://www.dailymotion.com/{USERNAME}'
+# ETSY
+etsy = f'https://www.etsy.com/shop/{USERNAME}'
+# CASHME
+cashme = f'https://cash.me/{USERNAME}'
+# BEHANCE
+behance = f'https://www.behance.net/{USERNAME}'
+# GOODREADS
+goodreads = f'https://www.goodreads.com/{USERNAME}'
+# INSTRUCTABLES
+instructables = f'https://www.instructables.com/member/{USERNAME}'
+# KEYBASE
+keybase = f'https://keybase.io/{USERNAME}'
+# KONGREGATE
+kongregate = f'https://kongregate.com/accounts/{USERNAME}'
+# LIVEJOURNAL
+livejournal = f'https://{USERNAME}.livejournal.com'
+# ANGELLIST
+angellist = f'https://angel.co/{USERNAME}'
+# LAST.FM
+last_fm = f'https://last.fm/user/{USERNAME}'
+# DRIBBBLE
+dribbble = f'https://dribbble.com/{USERNAME}'
+# CODECADEMY
+codecademy = f'https://www.codecademy.com/{USERNAME}'
+# GRAVATAR
+gravatar = f'https://en.gravatar.com/{USERNAME}'
+# PASTEBIN
+pastebin = f'https://pastebin.com/u/{USERNAME}'
+# FOURSQUARE
+foursquare = f'https://foursquare.com/{USERNAME}'
+# ROBLOX
+roblox = f'https://www.roblox.com/user.aspx?USERNAME={USERNAME}'
+# GUMROAD
+gumroad = f'https://www.gumroad.com/{USERNAME}'
+# NEWSGROUND
+newsground = f'https://{USERNAME}.newgrounds.com'
+# WATTPAD
+wattpad = f'https://www.wattpad.com/user/{USERNAME}'
+# CANVA
+canva = f'https://www.canva.com/{USERNAME}'
+# CREATIVEMARKET
+creative_market = f'https://creativemarket.com/{USERNAME}'
+# TRAKT
+trakt = f'https://www.trakt.tv/users/{USERNAME}'
+# 500PX
+five_hundred_px = f'https://500px.com/{USERNAME}'
+# BUZZFEED
+buzzfeed = f'https://buzzfeed.com/{USERNAME}'
+# TRIPADVISOR
+tripadvisor = f'https://tripadvisor.com/members/{USERNAME}'
+# HUBPAGES
+hubpages = f'https://{USERNAME}.hubpages.com'
+# CONTENTLY
+contently = f'https://{USERNAME}.contently.com'
+# HOUZZ
+houzz = f'https://houzz.com/user/{USERNAME}'
+#BLIP.FM
+blipfm = f'https://blip.fm/{USERNAME}'
+# WIKIPEDIA
+wikipedia = f'https://www.wikipedia.org/wiki/User:{USERNAME}'
+# HACKERNEWS
+hackernews = f'https://news.ycombinator.com/user?id={USERNAME}'
+# CODEMENTOR
+codementor = f'https://www.codementor.io/{USERNAME}'
+# REVERBNATION
+reverb_nation = f'https://www.reverbnation.com/{USERNAME}'
+# DESIGNSPIRATION
+designspiration = f'https://www.designspiration.net/{USERNAME}'
+# BANDCAMP
+bandcamp = f'https://www.bandcamp.com/{USERNAME}'
+# COLOURLOVERS
+colourlovers = f'https://www.colourlovers.com/love/{USERNAME}'
+# IFTTT
+ifttt = f'https://www.ifttt.com/p/{USERNAME}'
+# EBAY
+ebay = f'https://www.ebay.com/usr/{USERNAME}'
+# SLACK
+slack = f'https://{USERNAME}.slack.com'
+# OKCUPID
+okcupid = f'https://www.okcupid.com/profile/{USERNAME}'
+# TRIP
+trip = f'https://www.trip.skyscanner.com/user/{USERNAME}'
+# ELLO
+ello = f'https://ello.co/{USERNAME}'
+# TRACKY
+tracky = f'https://tracky.com/user/~{USERNAME}'
+# BASECAMP
+basecamp = f'https://{USERNAME}.basecamphq.com/login'
+#
+''' WEBSITE LIST - USE FOR SEARCHING OF USERNAME '''
+
+WEBSITES = [
+instagram, facebook, twitter, youtube, blogger, google_plus, reddit,
+wordpress, pinterest, github, tumblr, flickr, steam, vimeo, soundcloud, disqus,
+medium, deviantart, vk, aboutme, imgur, flipboard, slideshare, fotolog, spotify,
+mixcloud, scribd, badoo, patreon, bitbucket, dailymotion, etsy, cashme, behance,
+goodreads, instructables, keybase, kongregate, livejournal, angellist, last_fm,
+dribbble, codecademy, gravatar, pastebin, foursquare, roblox, gumroad, newsground,
+wattpad, canva, creative_market, trakt, five_hundred_px, buzzfeed, tripadvisor, hubpages,
+contently, houzz, blipfm, wikipedia, hackernews, reverb_nation, designspiration,
+bandcamp, colourlovers, ifttt, ebay, slack, okcupid, trip, ello, tracky, basecamp]
+
+
+
+
+#
